17537ab1d74769fd0c98712150f142fa27efa0a2
Prince <p.nkosi42@gmail.com>
2024-08-09 20:17:04 +0200

    Add Dockerfile for containerization

--- cv/apps.py
+++ cv/apps.py
@@ -1,6 +1,19 @@
+"""
+This module contains the configuration class for the 'cv' Django application.
+
+The CvConfig class is used to configure settings specific to the 'cv' application.
+It defines the default type of auto-generated primary key field for models 
+and the name of the application.
+"""
 from django.apps import AppConfig
 
 
 class CvConfig(AppConfig):
+    """Configuration class for the 'cv' Django application.
+
+    Args:
+        default_auto_field (str): The type of auto-generated primary key field to use for models.
+        name (str): The name of the application, which is 'cv' in this case.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cv'
--- cv/views.py
+++ cv/views.py
@@ -18,10 +18,22 @@ from .models import Experience, Education, Skill
 
 
 def logged_out(request):
+    """Renders the logged out template after user logout"""
     return render(request, 'registration/logged_out.html')
 
 def register(request):
-    """Handles user registration."""
+    """Handles user registration.
+    If the request method is POST, it process the registration form. If the form 
+    is valid, it saves the new user and redirects to the login page with a sucess
+    message. If the form has errors, it displays them. for GET request, it renders
+    the registration form. 
+    
+    Args:
+        request: The HTTP request object. 
+    
+    Returns:
+        HttpResponse: The rendered registratin page with the form
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -44,7 +56,17 @@ def cv_home(request):
 
 @login_required
 def cv_page(request):
-    """Renders the CV page with experience, education, and skills."""
+    """Renders the CV page with experience, education, and skills.
+
+    Retrieves all experience, education, and skill entries from the database and passes them
+    to the 'cv/cv_page.html' template.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered CV page template with the context.
+    """
     experiences = Experience.objects.all()
     education = Education.objects.all()
     skills = Skill.objects.all()
@@ -57,5 +79,14 @@ def cv_page(request):
 
 @login_required
 def contact(request):
-    """Renders the contact page."""
+    """Renders the contact page.
+
+    This view is accessible only to logged-in users and renders the 'cv/contact.html' template.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered contact page template.
+    """
     return render(request, 'cv/contact.html')
